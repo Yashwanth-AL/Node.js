195a6c4a47d6242adcc0b7ed773746265d88f1a2
Yashwanth A L <yashwanthal2004@gmail.com>
2024-10-11 23:03:40 +0530

    added npm modules

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// var generateName = require("sillyname");
+// import generateName from "sillyname";
+
+// var randomName = generateName();
+// console.log(`The random name genarated is ${randomName}.`);
+
+
+// import superheroes from "superheroes"
+// let name = superheroes.random();
+// console.log(`My name is : ${name}`);
+
+import { randomSuperhero } from 'superheroes';  
+let name = randomSuperhero();
+console.log(`My superhero name is: ${name}`);
